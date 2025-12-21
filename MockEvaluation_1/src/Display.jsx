0e3ea6772506dd7fef989b1eb34ce5b87c462379
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-12-21 19:12:07 +0530

    mockEvaluation1 on react creating posts which can be update,displayed,deleted using the useEffect and useState

--- MockEvaluation_1/src/Display.jsx
+++ MockEvaluation_1/src/Display.jsx
@@ -1,7 +1,14 @@
-function Display({posts}){
+function Display({posts,onDisplay,onEdit}){
     return(
         <>
-        {posts.map}
+        {posts.map((post)=>(
+            <div key={post.id}>
+                <h4>{post.title}</h4>
+                <p>{post.content}</p>
+            </div>
+        ))}
+        <button onClick={()=>onDisplay(posts)}>Display</button>
+        <button onClick={()=>onEdit(posts)}>Edit</button>
         </>
     )
 }
