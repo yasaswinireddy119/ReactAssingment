f6c0cc3022a6656c7494bc67a16223827b63d172
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-12-28 19:37:11 +0530

    fleet-management-react-app

--- MckEvaluation2/src/components/admin.jsx
+++ MckEvaluation2/src/components/admin.jsx
@@ -16,10 +16,66 @@ function Admin(){
     }
     const handleAddFleet=(e)=>{
         e.preventDefault();
-        setFleetData
+        setFleetData([...fleetData,fleet]);
+        setFleet({
+            registration_No:"",
+            category:"",
+            driver_name:"",
+            availability:""
+
+        })
     }
     return(
-        <h1>Admin Page</h1>
+        <div>
+        
+        <h3>Add Fleet</h3>
+        <form onSubmit={handleAddFleet}>
+            <input 
+            type="text"
+            name="registration_No"
+            placeholder="Vehicle reg no"
+            value={fleet.registration_No}
+            onChange={handleChange}/>
+
+            <br />
+            <br />
+            <select name="category"
+            value={fleet.category}
+            onChange={handleChange}/>
+            <br />
+            <br />
+            <select name="category"
+            value={fleet.category}
+            onChange={handleChange}>
+                <option value="">Select Category</option>
+                  <option value="Auto">Auto</option>
+                    <option value="Car">Car</option>
+                      <option value="Truck">Truck</option>
+                      <option value="Bus">Bus</option>
+            </select>
+            <br />
+            <br />
+            <input
+            type="text"
+            name="driver_name"
+            placeholder="Driver Name"
+            value={fleet.driver_name}
+            onChange={handleChange}/>
+
+            <br /><br />
+            <select 
+            name="availabilty"
+            value={fleet.availability}
+            onChange={fleet.handleChange}>
+                <option value="">Availabiltity</option>
+                  <option value="Availabile">Availabile</option>
+                    <option value="Unavailabile">Unavailabile</option>
+            </select>
+            <button type="submit">Add Fleet</button>
+        </form>
+        </div>
+
+        
 
     )
 }
