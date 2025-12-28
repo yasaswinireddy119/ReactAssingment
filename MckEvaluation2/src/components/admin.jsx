3747a94a5b9386d8d651e3719bab67be77f197b6
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-12-28 19:23:32 +0530

    fleet-management-react-app

--- MckEvaluation2/src/components/admin.jsx
+++ MckEvaluation2/src/components/admin.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 function Admin(){
+    const[fleet,setFleet]=useState({
+        registration_No:"",
+        category:"",
+        driver_name:"",
+        availability:""
+    })
+    const[fleetData,setFleetData]=useState([])
+    const handleChange = (e)=>{
+        setFleet({
+            ...fleet,
+            [e.target.name]:e.target.value
+    })
+    }
+    const handleAddFleet=(e)=>{
+        e.preventDefault();
+        setFleetData
+    }
     return(
         <h1>Admin Page</h1>
+
     )
 }
 export default Admin;
